e011fab934de02b67919a3645753818e6c6fee47
Vitaliy Popov <0702vitaliy@gmail.com>
2024-10-19 03:03:32 +0500

    init commit

---
+++ src/main/java/com/example/chattuna/ChatTunaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chattuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTunaApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(ChatTunaApplication.class, args);
+	}
+
+}
